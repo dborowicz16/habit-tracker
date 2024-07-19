ef5f4ff75615d4b7c008158abfa78aa23f578cf6
dborowicz16 <79418706+dborowicz16@users.noreply.github.com>
2024-07-19 16:34:03 -0400

    Return user login ID on front end

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ export default function App() {
     <Authenticator>
       {({ signOut, user }) => (
         <main>
-          <h1>My todos</h1>
+          <h1>{user?.signInDetails?.loginId}'s todos</h1>          
           <button onClick={createTodo}>+ new</button>
           <button onClick={signOut}>Sign out</button>
           <ul>
